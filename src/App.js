bcaae54ba7e1e1f32ce76adb5556ddf44730ccf1
Aditya Srivastava <adityandroid@gmail.com>
2019-09-03 17:50:01 +0530

    Final commits
    added flexbox

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
-import React from "react";
+import React from "react"
 
 class Card extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      quote: "Do what is meaningful not what is expedient",
-      author: "Jordan B Peterson"
-    };
+      quote: "",
+      author: ""
+    }
   }
 
+  componentDidMount() {
+    this.getQuote()
+    console.log("Get quote called in mount method")
+  }
   getQuote = () => {
     fetch("https://api.quotable.io/random")
       .then(response => response.json())
       .then(data => {
-        console.log(JSON.stringify(data));
-        this.setState({ quote: data.content, author: data.author });
+        console.log(JSON.stringify(data))
+        this.setState({ quote: data.content, author: data.author })
       })
       .catch(err => {
-        console.log(err);
-      });
-  };
+        console.log(err)
+      })
+  }
 
   render() {
     return (
-      <div>
-        <h1> {this.state.quote} </h1>
-        <h4> {this.state.author} </h4>
-        <button onClick={this.getQuote}> Another One </button>
+      <div id="container">
+        <div id="quote-box">
+          <div id="text"> {this.state.quote} </div>
+          <div id="author"> -{this.state.author} </div>
+          <div id="buttons">
+            <button id="new-quote" onClick={this.getQuote}>
+              Another One
+            </button>
+            <a
+              href={"https://twitter.com/intent/tweet?text=" + this.state.quote}
+              id="tweet-quote"
+              title="Tweet this quote"
+            >
+              <img
+                alt="Tweet this quote"
+                src="https://cdnjs.cloudflare.com/ajax/libs/webicons/2.0.0/webicons/webicon-twitter-m.png"
+              />
+            </a>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default Card;
+export default Card
